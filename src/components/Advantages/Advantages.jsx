434f2e05ac5b6bce9a40988f0394d88a3935c9af
Anastasiya Ruban <nastja21.91@mail.ru>
2023-06-12 20:25:42 +0300

    add counter

--- src/components/Advantages/Advantages.jsx
+++ src/components/Advantages/Advantages.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './advantages.module.css';
+import { CounterContent } from '../CounterContent/CounterContent';
 
 export function Advantages() {
   return (
@@ -7,14 +8,18 @@ export function Advantages() {
       <li className={styles.advantages__item}>
         <a href='##' className={styles.advantages__link}>
           <p className={styles.advantages__point}>мы</p>
-          <span className={styles.advantages__value}>1</span>
+          <span className={styles.advantages__value}>
+            {CounterContent(10, 1, 300)}
+          </span>
           <p className={styles.advantages__point}>на&nbsp;рынке</p>
         </a>
       </li>
       <li className={styles.advantages__item}>
         <a href='##' className={styles.advantages__link}>
           <p className={styles.advantages__point}>гарантируем</p>
-          <span className={styles.advantages__value}>50%</span>
+          <span className={styles.advantages__value}>
+            {CounterContent(0, 50, 300)}%
+          </span>
           <p className={styles.advantages__point}>безопасность</p>
         </a>
       </li>
@@ -22,7 +27,8 @@ export function Advantages() {
         <a href='##' className={styles.advantages__link}>
           <p className={styles.advantages__point}>календарик&nbsp;за</p>
           <span className={styles.advantages__value}>
-            2001<span className={styles['advantages__value-small']}>г.</span>
+            {CounterContent(1500, 2001, 300)}
+            <span className={styles['advantages__value-small']}>г.</span>
           </span>
           <p className={styles.advantages__point}>в&nbsp;подарок</p>
         </a>
@@ -30,7 +36,9 @@ export function Advantages() {
       <li className={styles.advantages__item}>
         <a href='##' className={styles.advantages__link}>
           <p className={styles.advantages__point}>путешествие</p>
-          <span className={styles.advantages__value}>597</span>
+          <span className={styles.advantages__value}>
+            {CounterContent(200, 597, 300)}
+          </span>
           <p className={styles.advantages__point}>дней</p>
         </a>
       </li>
---
+++ src/components/CounterContent/CounterContent.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+export function CounterContent(startNum, stopNum, duration) {
+  let [value, setValue] = useState({ startNum: Number(startNum), idT: null });
+  let idT = null;
+  console.log(startNum, stopNum, duration);
+  useEffect(() => {
+    console.log(idT);
+    clearTimeout(value.idT);
+    if (value.startNum == stopNum) {
+      return;
+    }
+
+    idT = setTimeout(() => {
+      if (startNum > stopNum) {
+        setValue({ startNum: Number(value.startNum) - 1, idT: idT });
+      } else {
+        setValue({ startNum: Number(value.startNum) + 1, idT: idT });
+      }
+    }, duration / Math.abs(startNum - stopNum));
+  }, [value]);
+
+  return <span>{value.startNum}</span>;
+}
