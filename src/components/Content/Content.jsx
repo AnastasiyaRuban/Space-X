4fb0c8f8d200e743491c649aecb2196232ea4986
Anastasiya Ruban <nastja21.91@mail.ru>
2023-06-12 21:27:15 +0300

    change folders for deploy

--- src/components/Content/Content.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './content.module.css';
-
-export function Content({ children }) {
-  return <main className={`${styles.content} container`}>{children}</main>;
-}
--- src/components/Layout/Layout.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './layout.module.css';
-
-export function Layout({ children }) {
-  return <div className={styles.layout}>{children}</div>;
-}
