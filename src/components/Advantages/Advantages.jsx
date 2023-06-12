024b4d23270852253ae95d89c092a658819bcd5b
Anastasiya Ruban <nastja21.91@mail.ru>
2023-06-12 08:26:47 +0300

    add link to card-advantages + fixed linear gradient

--- src/components/Advantages/Advantages.jsx
+++ src/components/Advantages/Advantages.jsx
@@ -5,26 +5,34 @@ export function Advantages() {
   return (
     <ul className={styles.advantages}>
       <li className={styles.advantages__item}>
-        <p className={styles.advantages__point}>мы</p>
-        <span className={styles.advantages__value}>1</span>
-        <p className={styles.advantages__point}>на&nbsp;рынке</p>
+        <a href='##' className={styles.advantages__link}>
+          <p className={styles.advantages__point}>мы</p>
+          <span className={styles.advantages__value}>1</span>
+          <p className={styles.advantages__point}>на&nbsp;рынке</p>
+        </a>
       </li>
       <li className={styles.advantages__item}>
-        <p className={styles.advantages__point}>гарантируем</p>
-        <span className={styles.advantages__value}>50%</span>
-        <p className={styles.advantages__point}>безопасность</p>
+        <a href='##' className={styles.advantages__link}>
+          <p className={styles.advantages__point}>гарантируем</p>
+          <span className={styles.advantages__value}>50%</span>
+          <p className={styles.advantages__point}>безопасность</p>
+        </a>
       </li>
       <li className={styles.advantages__item}>
-        <p className={styles.advantages__point}>календарик&nbsp;за</p>
-        <span className={styles.advantages__value}>
-          2001<span className={styles['advantages__value-small']}>г.</span>
-        </span>
-        <p className={styles.advantages__point}>в&nbsp;подарок</p>
+        <a href='##' className={styles.advantages__link}>
+          <p className={styles.advantages__point}>календарик&nbsp;за</p>
+          <span className={styles.advantages__value}>
+            2001<span className={styles['advantages__value-small']}>г.</span>
+          </span>
+          <p className={styles.advantages__point}>в&nbsp;подарок</p>
+        </a>
       </li>
       <li className={styles.advantages__item}>
-        <p className={styles.advantages__point}>путешествие</p>
-        <span className={styles.advantages__value}>597</span>
-        <p className={styles.advantages__point}>дней</p>
+        <a href='##' className={styles.advantages__link}>
+          <p className={styles.advantages__point}>путешествие</p>
+          <span className={styles.advantages__value}>597</span>
+          <p className={styles.advantages__point}>дней</p>
+        </a>
       </li>
     </ul>
   );
